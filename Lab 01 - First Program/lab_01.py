75a15ada787c925f8182b7c9d153a7c41c5a81a2
Perez <alejandro.perezcuerv@edu.uah.es>
2022-02-10 20:01:25 +0100

    me olvidé los puntos

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
-print("I suppose that the \"Make sure what you print uses proper English \" wasn\'t meant to be taken in such a literal way")
-print("However, i think it doesn\'t matter and i will do it any way")
-print("Why?, you may ask")
-print("I don\'t really know, but technically i have written this in english, but not in a proper english")
+print("I suppose that the \"Make sure what you print uses proper English \" wasn\'t meant to be taken in such a literal way.")
+print("However, i think it doesn\'t matter and i will do it any way.")
+print("Why?, you may ask.")
+print("I don\'t really know, but technically i have written this in english, but not in a proper english.")
